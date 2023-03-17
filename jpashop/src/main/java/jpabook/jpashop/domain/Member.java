014d210e010f57877f1dfe345b19e2b1ba4e47fa
rhrudska987 <rhrudska987@naver.com>
2023-03-17 21:44:06 +0900

    study

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +24,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>(); //초기화를 생성자에 하는게 아니리 필드에서 하기. 이게 null문제에서 안전해짐
 
