2988469babf33d0bb867b39f9ead42afe1ee197f
rhrudska987 <rhrudska987@naver.com>
2023-03-18 21:48:09 +0900

    지연 로딩과 조회 성능 최적화

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryDto.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.repository.order.simplequery;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        orderId = this.orderId;
+        name = this.name;
+        orderDate = this.orderDate;
+        orderStatus = this.orderStatus;
+        address = this.address;
+    }
+}
