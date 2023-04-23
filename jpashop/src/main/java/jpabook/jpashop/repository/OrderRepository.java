042246ed7f818fb5108c961adb9892e663b0af36
rhrudska987 <rhrudska987@naver.com>
2023-04-23 18:47:34 +0900

    order search

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -61,5 +62,4 @@ public class OrderRepository {
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery("select o from Order o join fetch o.member m join fetch o.delivery d", Order.class).getResultList();
     }
-
 }
